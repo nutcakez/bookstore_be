3e801d62652d4d664e72477fcd2a74538769326d
nutcakez <knorbert91@gmail.com>
2021-05-14 10:09:43 +0200

    initial upload

---
+++ src/main/java/com/bookstore/bookstorebe/BookstorebeApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstorebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorebeApplication.class, args);
+	}
+
+}
